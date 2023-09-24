19ac47252f842aad6b8e5b9fca0f481b7eb4a814
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2023-09-24 17:41:09 +0800

    更新 entity.ts

--- src/plugins/entity.ts
+++ src/plugins/entity.ts
@@ -1,14 +1,15 @@
+// todo customform gui
+// todo 根据选择的switch，刷新对应实体
 
-
-interface bak {
+interface Obj_Entity {
     [key: string]: Array<Entity>
 }
 
-function entity(): bak {
+function entity(): Obj_Entity {
     // 处理实体
     // 根据实体类型进行分类返回
     // 防疫值应该 key实体名 Array<entity>
-    const cache: bak = {};
+    const cache: Obj_Entity = {};
     mc.getAllEntities().forEach(en => {
         if (!Object.prototype.hasOwnProperty.call(cache, en.name)) {
             cache[en.name] = [];
@@ -18,4 +19,5 @@ function entity(): bak {
     return cache;
 }
 
-// entity()[0][0].despawn
+// entity()["0"][0].despawn()
+// 访问0分类下的第0个实体对象的despawn方法
